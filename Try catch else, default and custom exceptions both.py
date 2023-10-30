b381496d81b05c882613e2563600b287868e299b
Dipesh3050 <149353299+Dipesh3050@users.noreply.github.com>
2023-10-30 11:35:09 +0530

    Add files via upload

---
+++ Try catch else, default and custom exceptions both.py
@@ -0,0 +1,19 @@
+
+#try except else finally
+try:
+  f = open("demofile.txt")
+  try:
+    f.write("Lorum Ipsum")
+  except:
+    print("Something went wrong when writing to the file")
+  finally:
+    f.close()
+except:
+  print("Something went wrong when opening the file")
+
+#raise an exception
+x = -1
+
+if x < 0:
+  raise Exception("Sorry, no numbers below zero")
+
